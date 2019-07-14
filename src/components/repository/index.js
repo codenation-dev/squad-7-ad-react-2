d4bf322c0648fa1828493e1be0834d8b7109496b
leticiaenz <leticia.enz11@gmail.com>
2019-07-13 21:58:51 -0400

    add component Repository

---
+++ src/components/repository/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repository = ({ name, avatar }) => (
+  <div>
+    <h3>{name}</h3>
+    <h3>{avatar}</h3>
+  </div>
+);
+
+export default Repository;
