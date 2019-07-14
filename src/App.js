fcd0d42145170ea7975074a59a841d670c86a238
leticiaenz <leticia.enz11@gmail.com>
2019-07-13 21:58:21 -0400

    configure provider of store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './config/RectotronConfig';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
 
 export default App;
