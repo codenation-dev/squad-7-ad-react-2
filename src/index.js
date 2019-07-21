1f0ca8201c4bf12d9e1d522b5c51b8c69a81e207
Douglas Laiber <douglaslbosco@gmail.com>
2019-07-21 16:27:22 -0300

    convert to css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './styles/common.scss';
+import './styles/common.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
