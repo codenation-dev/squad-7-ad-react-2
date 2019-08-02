ea6092c60a0bfd5eef8c82d5397ef9a7b009d9a7
VictorSales <v.salesb@gmail.com>
2019-08-02 20:53:07 -0300

    Removing RepoOrder because the API has a sort qualifier
    https://help.github.com/en/articles/sorting-search-results

--- src/components/cardList/index.js
+++ src/components/cardList/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CardItem from '../cardItem';
 
-const RepoOrder = (a, b) => {
-  if (a.created_at < b.created_at) return 1;
-  if (a.created_at > b.created_at) return -1;
-  return 0;
-};
-
 const CardList = ({ repositories }) =>
   repositories.length > 0 &&
   repositories
-    .sort(RepoOrder)
     .map(repository => (
       <CardItem key={repository.id} repository={repository} />
     ));
