5987f156b0925aa102c3897aa75c866961dc31f9
leticiaenz <leticia.enz11@gmail.com>
2019-08-02 14:00:46 -0400

    connect component messageNotFound to redux to handle new search

--- src/components/messageNotFound/index.js
+++ src/components/messageNotFound/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Creators as RepositoriesActions } from '../../store/ducks/repositories';
+import { StyledMessage, Header, Text, Content } from './style';
 import SearchBar from '../searchbar';
 
-import { StyledMessage, Header, Text, Content } from './style';
+class MessageNotFound extends Component {
+  state = {
+    username: '',
+  };
 
-const MessageNotFound = ({ header, text }) => {
-  return (
-    <StyledMessage>
-      <Header>{header}</Header>
-      <Content>
-        <Text>{text}</Text>
-        <SearchBar />
-      </Content>
-    </StyledMessage>
-  );
-};
+  setUsername = value => {
+    this.setState({ username: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username } = this.state;
+    const { getRepositoriesRequest } = this.props;
+    getRepositoriesRequest(username);
+  };
+
+  render() {
+    const { header, text } = this.props;
+    const { username } = this.state;
+    return (
+      <StyledMessage>
+        <Header>{header}</Header>
+        <Content>
+          <Text>{text}</Text>
+          <SearchBar
+            onSubmit={this.handleSubmit}
+            keyword={username}
+            onChange={e => this.setUsername(e.target.value)}
+            isLoading={false}
+            text="github.com/"
+          />
+        </Content>
+      </StyledMessage>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(MessageNotFound);
 
 MessageNotFound.propTypes = {
   header: PropTypes.string,
   text: PropTypes.string,
+  getRepositoriesRequest: PropTypes.func.isRequired,
 };
 
 MessageNotFound.defaultProps = {
@@ -26,5 +63,3 @@ MessageNotFound.defaultProps = {
   text:
     'Não encontramos o usuário que você está buscando. Deseja realizar uma nova pesquisa?',
 };
-
-export default MessageNotFound;
